54f6ab595f98f89afcccdb4cf0ce52d1c27c7947
rakibhossainraju <rakibhossianraju@gmail.com>
2024-11-04 15:06:11 +0600

    Added more solution

---
+++ problem-solving/compressed-string.ts
@@ -0,0 +1,51 @@
+function compressedString(word: string): string {
+  let prevChar = word[0];
+  let charCount = 1;
+  let comp = "";
+
+  for (let i = 1; i <= word.length; i++) {
+    if (prevChar === word[i] && word[i] !== undefined) {
+      if (charCount < 9) charCount++;
+      else {
+        comp += charCount.toString() + prevChar;
+        charCount = 1;
+      }
+    } else {
+      comp += charCount.toString() + prevChar;
+      charCount = 1;
+    }
+    prevChar = word[i];
+  }
+
+  return comp;
+}
+
+console.log(compressedString("abcde"));
+console.log(compressedString("aaaaaaaaaaaaaabb"));
+
+function compress(chars: string[]): number {
+    let prevChar = chars[0];
+    let charCount = 1;
+    let output = "";
+  
+    for (let i = 1; i <= chars.length; i++) {
+      if (prevChar === chars[i] && chars[i] !== undefined) {
+        charCount++;
+      } else {
+        if(charCount === 1) output += prevChar;
+        else {
+            output += (prevChar + charCount.toString());
+        };
+        charCount = 1;
+      }
+      prevChar = chars[i];
+    }
+    for(let i = 0; i < output.length; i++){
+        chars[i] = output[i];
+    }
+    
+    return output.length;
+}
+
+console.log(compress(["a","a","b","b","c","c","c"]));
+console.log(compress(["a","b","b","b","b","b","b","b","b","b","b","b","b"]));
---
+++ problem-solving/count-segments.ts
@@ -0,0 +1,20 @@
+function countSegments(s: string): number {
+    if(!s.length) return 0;
+    const output = []
+    let tempStr = "";
+    
+    for(const char of s){
+        if(char === " "){
+            if(tempStr.length) output.push(tempStr);
+            tempStr = "";
+        }else {
+            tempStr += char;
+        }
+    }
+    if(tempStr.length) output.push(tempStr);
+    return output.length;
+};
+
+console.log(countSegments("Hello, my name is John"));
+console.log(countSegments('   Hello    '));
+console.log(countSegments("                "));
