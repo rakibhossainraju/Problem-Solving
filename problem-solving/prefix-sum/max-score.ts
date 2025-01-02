c03f9ff90d3ee475cd8157e258db2ba1bc21844b
rakibhossainraju <rakibhossianraju@gmail.com>
2025-01-02 12:38:02 +0600

    Added tow new problem and a problem category.

---
+++ problem-solving/prefix-sum/max-score.ts
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/maximum-score-after-splitting-a-string/description/
+function maxScore(s: string): number {
+  const totalOnes = (s.match(/1/g)?.length ?? 0)
+  let leftZeros = 0;
+  let leftOnes = 0;
+  let maxScore = 0;
+  for(let i = 0; i < s.length - 1; ++i){
+    if(s[i] === '0') leftZeros++;
+    else leftOnes++;
+
+    const rightOnes = totalOnes - leftOnes;
+    const currentScore = leftZeros + rightOnes;
+    maxScore = Math.max(maxScore, currentScore);
+  }
+  return maxScore
+};
---
+++ problem-solving/prefix-sum/vowel-strings.ts
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/count-vowel-strings-in-ranges/
+
+function vowelStrings(words: string[], queries: number[][]): number[] {
+  const vowels = new Set<Readonly<string>>(["a", "e", "i", "o", "u"]);
+  let previousValue = 0;
+  const prefixSum: number[] = Array.from({ length: words.length }, (_, i) => {
+    if (i === 0) {
+      previousValue = vowels.has(words[0][0]) && vowels.has(words[0][words[0].length - 1]) ? 1 : 0;
+    } else {
+      previousValue +=
+        vowels.has(words[i][0]) && vowels.has(words[i][words[i].length - 1])
+          ? 1
+          : 0;
+    }
+
+    return previousValue;
+  });
+  const answer: number[] = [];
+  for (const [li, ri] of queries) {
+    const count = prefixSum[ri] - (li > 0 ? prefixSum[li - 1] : 0);
+    answer.push(count);
+  }
+
+  return answer;
+}
